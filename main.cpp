50f5d3c9471c20ca1c9fccd1848d8849ee624f01
Taylan Smith <possiblyremember@gmail.com>
2025-10-20 22:13:50 -0400

    fixing and improving

---
+++ main.cpp
@@ -0,0 +1,245 @@
+﻿#pragma region BASIC_INITIALIZATION
+
+#include "main.hpp"
+
+using glm::vec3;
+using std::vector;
+
+int width = 200, height = 200;
+
+float lastFrame = 0.0f;
+float deltaTime = 0.0f;
+
+vec3 cameraPos = vec3(0.0f, 0.0f, 3.0f);
+vec3 cameraUp = vec3(0.0f, 1.0f, 0.0f);
+vec3 cameraFront = vec3(0.0f, 0.0f, -1.0f);
+
+glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)width / (float)height, 0.1f, 100.0f);
+
+vector<Game::Geometry*> geometryObjects;
+
+
+#pragma endregion
+
+
+#pragma region SHADERS
+const char* vertexShaderSource = R"(
+
+#version 330 core
+layout (location = 0) in vec3 aPos;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+out vec3 vertexColor;
+out float depthVal;
+
+void main() {
+    vec4 viewSpacePos = view * model * vec4(aPos, 1.0);
+    depthVal = -viewSpacePos.z / 10.0; // adjust 50.0 to your scene scale
+    depthVal = clamp(depthVal, 0.0, 1.0);
+
+    vec3 finalColor = (1.0 - depthVal) ; // darkens by depth
+    vertexColor = finalColor;
+
+    gl_Position = projection * viewSpacePos; // determine position in clip space
+})";
+
+const char* fragmentShaderSource = R"(
+#version 330 core
+out vec4 FragColor;
+
+void main() {
+    FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+})";
+#pragma endregion
+
+
+#pragma region GAME_OBJECTS
+// redirected to hpp
+#pragma endregion
+
+#pragma region WINDOW_SIZE_MANAGEMENT
+
+void framebuffer_size_callback(GLFWwindow* window, int w, int h) {
+    glViewport(0, 0, w, h);
+    projection = glm::perspective(glm::radians(45.0f), (float)w / (float)h, 0.1f, 100.0f);
+}
+
+#pragma endregion
+
+#pragma region MOUSE_CONTROLS
+
+float yaw = -90.0f; // yaw is initialized to -90.0 degrees so that the camera points initially along -Z.
+float pitch = 0.0f;
+float lastX = width / 2.0;
+float lastY = height / 2.0;
+bool firstMouse = true;
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+    static float sensitivity = 0.1f;
+    if (firstMouse) {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+    lastX = xpos;
+    lastY = ypos;
+
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw += xoffset;
+    pitch += yoffset;
+
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+
+    glm::vec3 front;
+    front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    front.y = sin(glm::radians(pitch));
+    front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    cameraFront = glm::normalize(front);
+}
+
+#pragma endregion
+
+#pragma region KEYBOARD_CONTROLS 
+
+void processInput(GLFWwindow* window) {
+    float cameraSpeed = 2.5f * deltaTime; // adjust speed
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+        cameraPos -= cameraUp * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+        cameraPos += cameraUp * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, 1);
+}
+
+#pragma endregion
+
+
+#pragma region SHADER_UTILS
+unsigned int compileShader(unsigned int type, const char* source) {
+    unsigned int shader = glCreateShader(type);
+    glShaderSource(shader, 1, &source, nullptr);
+    glCompileShader(shader);
+    return shader;
+}
+
+unsigned int createShaderProgram() {
+    unsigned int vertex = compileShader(GL_VERTEX_SHADER, vertexShaderSource);
+    unsigned int fragment = compileShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
+    unsigned int program = glCreateProgram();
+    glAttachShader(program, vertex);
+    glAttachShader(program, fragment);
+    glLinkProgram(program);
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+    return program;
+}
+#pragma endregion
+
+
+int main() {
+#pragma region GLFW INIT
+    if (!glfwInit()) {
+        std::cerr << "GLFW failed to init.\n";
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(width, height, "Basic Game", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create window\n";
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+	glfwFocusWindow(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSwapInterval(1);
+#pragma endregion
+#pragma region GLAD INIT
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to init GLAD\n";
+        return -1;
+    }
+
+    // After window setup, set up rendering params and reqs
+    glEnable(GL_DEPTH_TEST);
+#pragma endregion
+
+    //Game::Cube* cube = new Game::Cube(1.0f);
+	//geometryObjects.push_back(cube);
+
+    Game::Geometry* monkey = new Game::Geometry("C:\\Users\\tis\\Documents\\monkey.fbx");
+	geometryObjects.push_back(monkey);
+
+    unsigned int shaderProgram = createShaderProgram();
+
+
+
+
+    while (!glfwWindowShouldClose(window)) {
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        processInput(window);
+
+        glClearColor(0.1f, 0.1f, 0.15f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+
+        glm::mat4 model = glm::mat4(1.0f);
+        glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+
+        // Send uniforms
+        GLuint modelLoc = glGetUniformLocation(shaderProgram, "model");
+        GLuint viewLoc = glGetUniformLocation(shaderProgram, "view");
+        GLuint projLoc = glGetUniformLocation(shaderProgram, "projection");
+
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+        for (Game::Geometry* geometry : geometryObjects) {
+			geometry->draw(shaderProgram);
+        }
+
+
+
+        //cube.draw(shaderProgram);
+
+        //monkey.draw(shaderProgram);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    for (Game::Geometry* geometry : geometryObjects) {
+        delete geometry;
+	}
+    glfwTerminate();
+    return 0;
+}
---
+++ main.hpp
@@ -0,0 +1,192 @@
+﻿// basic_game_LOVE_CMAKE_SMILE.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+#include <iostream>
+#include <cmath>
+#include <print>
+
+using glm::vec3;
+using std::vector;
+
+namespace Game{
+	struct Transform {
+		glm::vec3 position{ 0.0f };
+		glm::quat rotation{ 1.0f, 0.0f, 0.0f, 0.0f }; // rotation as quaternion (THIS IS IMPORTANT)
+		glm::vec3 scale{ 1.0f };
+	};
+	struct Vertex {
+		glm::vec3 position;
+		glm::vec3 normal;
+		glm::vec2 texCoords;
+	};
+
+	class GameObject {
+	public:
+		Transform transform; // Transform for position, rotation, and scale
+
+		static glm::vec3 ConvertQuatToEuler(const glm::quat& q) {
+            // Convert quaternion to Euler angles (in radians)
+            float roll = atan2(2.0f * (q.w * q.x + q.y * q.z), 1.0f - 2.0f * (q.x * q.x + q.y * q.y));
+            float pitch = asin(2.0f * (q.w * q.y - q.z * q.x));
+            float yaw = atan2(2.0f * (q.w * q.z + q.x * q.y), 1.0f - 2.0f * (q.y * q.y + q.z * q.z));
+            return glm::vec3(pitch, yaw, roll);
+        }
+		static glm::quat ConvertEulerToQuat(const glm::vec3& euler) {
+            // Convert Euler angles (in radians) to quaternion
+            float cr = cos(euler.x * 0.5f);
+            float sr = sin(euler.x * 0.5f);
+            float cp = cos(euler.y * 0.5f);
+            float sp = sin(euler.y * 0.5f);
+            float cy = cos(euler.z * 0.5f);
+            float sy = sin(euler.z * 0.5f);
+            glm::quat q;
+            q.w = cr * cp * cy + sr * sp * sy;
+            q.x = sr * cp * cy - cr * sp * sy;
+            q.y = cr * sp * cy + sr * cp * sy;
+            q.z = cr * cp * sy - sr * sp * cy;
+            return q;
+        }
+
+        GameObject() {
+            transform.position = glm::vec3(0.0f);
+            transform.rotation = glm::quat(1.0f, 0.0f, 0.0f, 0.0f);
+            transform.scale = glm::vec3(1.0f);
+        }
+        virtual ~GameObject() {}
+
+	private:
+
+	};
+
+    class Geometry : public Game::GameObject {
+    public:
+        vector<vec3> vertices;
+        vector<unsigned int> indices;
+
+        GLuint vao = 0, vbo = 0, ebo = 0;
+
+
+        Geometry() {
+        }
+
+        Geometry(const std::string& path) {
+
+            Assimp::Importer importer; // initalize model importer and process model
+            const aiScene* scene = importer.ReadFile(path,
+                aiProcess_Triangulate |
+                aiProcess_GenNormals |
+                aiProcess_FlipUVs |
+                aiProcess_JoinIdenticalVertices);
+
+            if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) {
+                std::cerr << "Assimp Error: " << importer.GetErrorString() << std::endl;
+                return;
+            }
+
+            for (unsigned int m = 0; m < scene->mNumMeshes; m++) {
+                aiMesh* mesh = scene->mMeshes[m];
+                for (unsigned int i = 0; i < mesh->mNumVertices; i++) {
+                    // Positions
+                    vertices.push_back(vec3(mesh->mVertices[i].x, mesh->mVertices[i].y, mesh->mVertices[i].z));
+                }
+
+                // Indices
+                for (unsigned int i = 0; i < mesh->mNumFaces; i++) {
+                    aiFace face = mesh->mFaces[i];
+                    for (unsigned int j = 0; j < face.mNumIndices; j++)
+                        indices.push_back(face.mIndices[j]);
+                }
+            }
+
+            upload();
+        }
+
+
+        virtual ~Geometry() {
+            if (ebo) glDeleteBuffers(1, &ebo);
+            if (vbo) glDeleteBuffers(1, &vbo);
+            if (vao) glDeleteVertexArrays(1, &vao);
+
+
+        }
+
+        void upload() {
+            glGenVertexArrays(1, &vao);
+            glGenBuffers(1, &vbo);
+            glBindVertexArray(vao);
+
+            // Upload vertex data 
+            glBindBuffer(GL_ARRAY_BUFFER, vbo);
+            glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(vec3), vertices.data(), GL_STATIC_DRAW);
+
+            if (!indices.empty()) {
+                glGenBuffers(1, &ebo);
+                glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+                glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
+            }
+
+            /*glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+            glEnableVertexAttribArray(0);
+            glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+            glEnableVertexAttribArray(1);*/
+
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+            glEnableVertexAttribArray(0);
+
+
+            glBindVertexArray(0);
+        }
+
+        void draw(GLuint shaderProgram) {
+            glUseProgram(shaderProgram);
+            glBindVertexArray(vao);
+
+            if (!indices.empty()) {
+                glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(indices.size()), GL_UNSIGNED_INT, 0);
+            }
+            else {
+                glDrawArrays(GL_TRIANGLES, 0, static_cast<GLsizei>(vertices.size() / 3));
+            }
+
+            glBindVertexArray(0);
+        }
+    };
+
+    class Cube : public Geometry {
+    public:
+        Cube(float scale = 1) {
+            vertices = {
+                // positions         
+               {-0.5f*scale, -0.5f*scale, -0.5f*scale},
+               { 0.5f*scale, -0.5f*scale, -0.5f*scale},
+               { 0.5f*scale,  0.5f*scale, -0.5f*scale},
+               {-0.5f*scale,  0.5f*scale, -0.5f*scale},
+               {-0.5f*scale, -0.5f*scale,  0.5f*scale},
+               { 0.5f*scale, -0.5f*scale,  0.5f*scale},
+               { 0.5f*scale,  0.5f*scale,  0.5f*scale},
+               {-0.5f*scale,  0.5f*scale,  0.5f*scale}
+            };
+
+            indices = {
+                0, 1, 2, 2, 3, 0,
+                4, 5, 6, 6, 7, 4,
+                0, 1, 5, 5, 4, 0,
+                2, 3, 7, 7, 6, 2,
+                1, 2, 6, 6, 5, 1,
+                3, 0, 4, 4, 7, 3
+            };
+
+            upload();
+        }
+    };
+}
